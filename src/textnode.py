733d6c8e2079bbb847002018fa1faba36f05e761
jamesonhm <jamesonhmarshall@gmail.com>
2024-04-02 06:15:51 -0600

    added text to leaf fn

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,6 @@
+from htmlnode import LeafNode
+
+
 class TextNode:
 
     def __init__(self, text, text_type, url=None):
@@ -10,3 +13,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", '', {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception(f"Invalid text type {text_node.text_type}")
+
